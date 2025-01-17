8297224e1af715f988e2f312b0471dadbe67294a
alisherodilov2 <alisherodilov741@gmail.com>
2025-01-17 17:08:05 +0500

    code cleaned routes bug fixed

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,16 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('my-login', views.my_login, name='my-login'),
     path('logout', views.logout_view, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard', products_controller.ProductsController.index, name='dashboard'),
     path('register', views.register, name='register'),
-    # create new product
-    path('create-product', views.create_product, name='create-product'),
-    # path('get-all-products', views.get_all_products, name='get-all-products'),
     path('products', products_controller.ProductsController.get_all, name='get-all-products'),
     path('products/create', products_controller.ProductsController.create, name='create-product'),
-    path('products/dashboard', products_controller.ProductsController.index, name='dashboard'),
 ]
-
 
 
 
